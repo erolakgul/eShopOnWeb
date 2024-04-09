deae4c32690284ca0ec51e1664d6466c69528ae4
erol.akgul <erolakgul88@gmail.com>
2024-04-09 18:57:33 +0300

    -order endpoint için order id verilip değer alınıp alınmadığını test eden methodu  PublicApiIntegrationTests.OrderEndpoints altına yazıldı.

---
+++ tests/PublicApiIntegrationTests/OrderEndpoints/OrderGetByIdEndpointTest.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+using BlazorAdmin.Services;
+using BlazorShared;
+using BlazorShared.Models.Orders;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PublicApiIntegrationTests.OrderEndpoints;
+
+[TestClass]
+public class OrderGetByIdEndpointTest
+{
+    private  HttpService _httpService;
+    public OrderGetByIdEndpointTest()
+    {
+        //// Arrange
+        BaseUrlConfiguration baseUrlConfiguration = new()
+        {
+            ApiBase = "https://localhost:5099/api/"
+        };
+
+        IOptions<BaseUrlConfiguration> options = Options.Create(baseUrlConfiguration);  
+
+       ToastService toastService = new();
+        _httpService = new(new System.Net.Http.HttpClient(), options, toastService);
+    }
+
+    [TestMethod]
+    public async Task ReturnsItemGivenValidId()
+    {
+        //// Arrange
+        const int id = 3;
+
+        //// Act
+        var response = await _httpService.HttpGet<EditOrderResult>($"order-items/{id}");
+
+        //// Assert
+        if (response is not null)
+        {
+            System.Console.WriteLine($"sipariş geçen => {response.Order.BuyerId}");
+
+            Assert.AreEqual(id, response!.Order.Id);
+        }
+        else
+        {
+            Assert.IsNull(response);    
+        }
+    }
+}
